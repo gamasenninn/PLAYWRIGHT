914a629c7073c5a074fa63bdd54537197bfabe1c
gamasenninn <gamasenninn@yahoo.co.jp>
2021-11-23 17:07:53 +0900

    yahoo scr

--- yahoo_scr.py
+++ yahoo_scr.py
@@ -2,6 +2,8 @@ from playwright.sync_api import Playwright, sync_playwright
 import math
 
 HEADLESS = True
+CATEGORY = '2084244599'  #トラクター車体
+PERPAGE = 100
 
 
 def run(playwright: Playwright) -> None:
@@ -12,23 +14,21 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
 
     # Go to https://www.yahoo.co.jp/
-    page.goto("https://auctions.yahoo.co.jp/search/search?auccat=&tab_ex=commerce&ei=utf-8&aq=-1&oq=&sc_i=&fr=auc_top&p=トラクター+馬力&fixed=2&x=0&y=0")
+    page.goto(f"https://auctions.yahoo.co.jp/search/search?auccat={CATEGORY}&tab_ex=commerce&ei=utf-8&aq=-1&oq=&sc_i=&fr=auc_top&fixed=2&x=0&y=0")
 
     loc = page.locator('.Tab__subText')
     l_list = loc.all_text_contents()
-    n= 100
-    max_page = math.ceil(int(l_list[1].replace(',','').replace('件',''))/100)
+    max_page = math.ceil(int(l_list[1].replace(',','').replace('件',''))/PERPAGE)
     print("max page:", max_page)
     for i in range(max_page):
-        b= n*i +1
-        print(b,n)
+        b= PERPAGE*i +1
+        print(b,PERPAGE)
         with page.expect_navigation():
-            html = page.content()
-            with open(f'yahoo_data/{i}.html',mode='w',encoding='utf-8') as f:
-                f.write(html)
-            #if i <= max_page -1 : 
-            page.screenshot(path=f"yahoo_data/{i}.png")
-            page.goto(f"https://auctions.yahoo.co.jp/search/search?p=トラクター+馬力&fixed=2&exflg=1&b={b}&n={n}")
+            page.goto(f"https://auctions.yahoo.co.jp/search/search?auccat={CATEGORY}&fixed=2&exflg=1&b={b}&n={PERPAGE}")
+        html = page.content()
+        with open(f'yahoo_data/{i+1}.html',mode='w',encoding='utf-8') as f:
+            f.write(html)
+        #page.screenshot(path=f"yahoo_data/{i+1}.png")
 
     # Close page
     print("Loop end.....")
